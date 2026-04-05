8ad8776fadcb088d2d21460ebb9a476e02598641
Beela303 <devbeela@gmail.com>
2026-04-05 02:41:57 +0100

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Must have /client
+import { createRoot, ReactDOM } from 'react-dom/client'; // Must have /client
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App';
 import StudentDetail from './pages/StudentDetail';
 import StudentsList from './pages/StudentsList';
 import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -13,7 +21,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/students" element={<StudentsList />} /> {/* New Route */}
+      <Route path="/students" element={<StudentsList />} />
       <Route path="/student/:id" element={<StudentDetail />} />
     </Routes>
   </BrowserRouter>
